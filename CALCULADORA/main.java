61c6120201bb9316c28cd5d10b21fbea0a74c5f6
Anderson Junior <92762017+JuniorSiqueir@users.noreply.github.com>
2025-05-18 17:56:46 -0300

    Coloquei repetição

--- CALCULADORA/main.java
+++ CALCULADORA/main.java
@@ -6,37 +6,48 @@ public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int escolha = 0;
-
+        String continuar;
         numero calculos = new numero();
-        System.out.println("Escolha o primeiro numero");
-        calculos.numeroA = scanner.nextInt();
-        System.out.println("Escolha o segundo numero");
-        calculos.numeroB = scanner.nextInt();
 
-        System.out.println("ESCOLHA A OPERAÇÃO");
-        System.out.println("1 - SOMAR");
-        System.out.println("2 - SUBTRAIR");
-        System.out.println("3 - DIVIDIR");
-        System.out.println("4 - MULTIPLICAR");
-        escolha = scanner.nextInt();
-
-        switch (escolha){
-            case 1:
-                calculos.somar();
-                break;
-            case 2:
-                calculos.subtrair();
-                break;
-            case 3:
-                calculos.dividir();
-                break;
-            case 4:
-                calculos.multiplicar();
-                break;
-            default:
-                System.out.println("OPÇÃO INVALIDA!!!!!");
-        }
+        do {
 
 
+            System.out.println("Escolha o primeiro numero");
+            calculos.numeroA = scanner.nextInt();
+            System.out.println("Escolha o segundo numero");
+            calculos.numeroB = scanner.nextInt();
+
+            System.out.println("ESCOLHA A OPERAÇÃO");
+            System.out.println("1 - SOMAR");
+            System.out.println("2 - SUBTRAIR");
+            System.out.println("3 - DIVIDIR");
+            System.out.println("4 - MULTIPLICAR");
+            escolha = scanner.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    calculos.somar();
+                    break;
+                case 2:
+                    calculos.subtrair();
+                    break;
+                case 3:
+                    calculos.dividir();
+                    break;
+                case 4:
+                    calculos.multiplicar();
+                    break;
+                default:
+                    System.out.println("OPÇÃO INVALIDA!!!!!");
+
+            }
+
+            System.out.println("DESEJA FAZER UM NOVO CALCULO? (S/N)");
+            scanner.nextLine(); // Limpa o buffer
+            continuar = scanner.nextLine();
+
+
+        } while (continuar.equalsIgnoreCase("s"));
+        System.out.println("ENCERRANDO PROGRAMA");
     }
 }
